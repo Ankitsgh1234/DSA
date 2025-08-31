7b4b002134c2575321051b5a0cb2119ca7240bfe
Ankit kumar Singh <ankit2005sgh@gmail.com>
2025-08-31 11:28:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (42.69%) - LeetSync

---
+++ 909-stone-game/stone-game.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int n=piles.length;
+        
+        int even=0;
+        int odd=0;
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                even+=piles[i];
+
+            }
+            else
+            odd+=piles[i];
+        }
+        return (even>odd||odd>even);
+    }
+}
