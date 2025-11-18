f7281180c83c5cae7bc706e42bea9600ded9e1ee
Ankit kumar Singh <ankit2005sgh@gmail.com>
2025-11-18 23:16:00 +0530

    Time: 1 ms (65.61%) | Memory: 47.5 MB (5.34%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,46 +1,44 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int  m= matrix.length;
-        int n = matrix[0].length;
-         
-        int[][] nm=new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                  nm[i][j]=-1;
+        int n=matrix.length;
+        int m=matrix[0].length;
+
+        int[][] arr=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]=-1;
             }
         }
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                     
                     int l=0;
-                    int k=0;
-                     while(l<n)
-                     {
-                        nm[i][l]=0;
+                    int u=0;
+                    while(l<n){
+                        arr[l][j]=0;
                         l++;
-
-                     }
-                     while(k<m)
-                     {
-                        nm[k][j]=0;
-                        k++;
-
-                     }
-
+                        
+                    }
+                    while(u<m){
+                        arr[i][u]=0;
+                        u++;
+                    }
                 }
-                else if (nm[i][j]==-1){
-                    nm[i][j]=matrix[i][j];
+                else if(arr[i][j]!=0){
+                    arr[i][j]=matrix[i][j];
                 }
-                 
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                matrix[i][j]= nm[i][j];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                matrix[i][j]=arr[i][j];
             }
         }
+        
+
+
         
     }
 }
