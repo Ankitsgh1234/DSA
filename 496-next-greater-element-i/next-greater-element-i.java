300731233026773db10aa727455ec3a696a88631
Ankit kumar Singh <ankit2005sgh@gmail.com>
2025-11-24 09:43:16 +0530

    Time: 3 ms (87.78%) | Memory: 44.6 MB (40.63%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,30 +1,24 @@
-import java.util.*;
-
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map = new HashMap<>(); // num -> next greater
-        Stack<Integer> stack = new Stack<>();
-
-        // Traverse nums2 from left to right
-        for (int num : nums2) {
-            // If current num is greater than top of stack
-            while (!stack.isEmpty() && num > stack.peek()) {
-                map.put(stack.pop(), num);
+        int n=nums1.length;
+        int m=nums2.length;
+        
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            int num=nums1[i];
+            int j=0;
+            while(nums2[j]!=num){
+                j++;
             }
-            stack.push(num);
+            while(j<m){
+                if(nums2[j]>num){
+                    arr[i]=nums2[j];
+                    break;
+                }
+                j++;
+                    arr[i]=-1;
+            }
         }
-
-        // For remaining numbers in stack, no next greater -> -1
-        while (!stack.isEmpty()) {
-            map.put(stack.pop(), -1);
-        }
-
-        // Build result for nums1
-        int[] result = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            result[i] = map.get(nums1[i]);
-        }
-
-        return result;
+        return arr;
     }
 }
