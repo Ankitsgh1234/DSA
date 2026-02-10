578adbf3b96101618808462607fa4de70d58e4b3
Ankit kumar Singh <ankit2005sgh@gmail.com>
2026-02-10 13:45:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (98.47%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,6 +1,8 @@
 class Solution {
     public int rob(int[] nums) {
 
+        // Method 1
+        /*
         int n=nums.length;
         if(n==1){
             return nums[0];
@@ -18,9 +20,18 @@ class Solution {
             dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
         }
         return dp[n-1];
+        */
       
-        
+        // Method-2
         // return robb(nums,0);
+
+        // Method-3
+
+        int n=nums.length;
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+
+        return mincost(nums,0,dp);
     }
     // public static int robb(int [] arr,int i){
     //     int n=arr.length;
@@ -33,4 +44,18 @@ class Solution {
     //     return Math.max(op1,op2);
 
     // }
+
+    public static int mincost(int[] nums,int i,int[] dp){
+         int n=nums.length;
+        if(i>=n){
+            return 0 ;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int op1=0+mincost(nums,i+1,dp);
+        int op2=nums[i]+mincost(nums,i+2,dp);
+
+        return dp[i]=Math.max(op1,op2);
+    }
 }
