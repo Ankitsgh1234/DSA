562b1f1497aa2c06e7318196cd4c3e1841cdbba1
Ankit kumar Singh <ankit2005sgh@gmail.com>
2026-02-10 12:30:38 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (33.84%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,20 +1,36 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 1) return nums[0];
 
-        int prev2 = nums[0];                        // dp[i-2]
-        int prev1 = Math.max(nums[0], nums[1]);    // dp[i-1]
-
-        for (int i = 2; i < nums.length; i++) {
-            int pick = prev2 + nums[i];            // choose this house
-            int notPick = prev1;                   // skip this house
-
-            int curr = Math.max(pick, notPick);
-
-            prev2 = prev1;
-            prev1 = curr;
+        int n=nums.length;
+        if(n==1){
+            return nums[0];
+        }
+        if(n==2){
+            return Math.max(nums[0],nums[1]);
         }
 
-        return prev1;
+        int[] dp=new int[n+1];
+
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[1],nums[0]);
+
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
+        }
+        return dp[n-1];
+      
+        
+        // return robb(nums,0);
     }
+    // public static int robb(int [] arr,int i){
+    //     int n=arr.length;
+    //     if(i>=n){
+    //         return 0 ;
+    //     }
+    //     int op1=0+robb(arr,i+1);
+    //     int op2=arr[i]+robb(arr,i+2);
+
+    //     return Math.max(op1,op2);
+
+    // }
 }
