5af476ea6387d0fcd40cef8f0d2a3d76eb24e5ad
Ankit kumar Singh <ankit2005sgh@gmail.com>
2026-02-15 01:25:38 +0530

    Time: 22 ms (48.84%) | Memory: 54.1 MB (50.48%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,27 +1,39 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int m=text1.length();
-        int n=text2.length();
+    public int longestCommonSubsequence(String s1, String s2) {
+        int m=s1.length();
+        int n=s2.length();
 
-        int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
-        Arrays.fill(dp[i],-1);
+        int[][] dp=new int[m+1][n+1];
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(i==0||j==0){
+                    dp[i][j]=0;
+                    
+                }
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
         }
+        
 
-        return lcs(m-1,n-1,text1,text2,dp);
+        return dp[m][n];
         
     }
-    public static int lcs(int m,int n,String s1,String s2,int[][] dp){
-        if(n==-1||m==-1){
-            return 0;
-        }
-        if(dp[m][n]!=-1){
-            return dp[m][n];
-        }
-        if(s1.charAt(m)==s2.charAt(n)){
-            return dp[m][n]= 1+lcs(m-1,n-1,s1,s2,dp);
-        }
-        return dp[m][n]= Math.max( lcs(m-1,n,s1,s2,dp),lcs(m,n-1,s1,s2,dp));
+    // public static int lcs(int m,int n,String s1,String s2,int[][] dp){
+    //     if(n==-1||m==-1){
+    //         return 0;
+    //     }
+    //     if(dp[m][n]!=-1){
+    //         return dp[m][n];
+    //     }
+    //     if(s1.charAt(m)==s2.charAt(n)){
+    //         return dp[m][n]= 1+lcs(m-1,n-1,s1,s2,dp);
+    //     }
+    //     return dp[m][n]= Math.max( lcs(m-1,n,s1,s2,dp),lcs(m,n-1,s1,s2,dp));
 
-    }
+    // }
 }
