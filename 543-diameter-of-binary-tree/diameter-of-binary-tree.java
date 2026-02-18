af58e8b773e4c326b77a6ac0a75652431e8a12ee
Ankit kumar Singh <ankit2005sgh@gmail.com>
2026-02-18 13:27:21 +0530

    Time: 266 ms (5.07%) | Memory: 47.8 MB (7.09%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -14,6 +14,25 @@
  * }
  */
 class Solution {
+    Map<TreeNode,Integer> map;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        map=new HashMap<>();
+        int a=diameterOfBinaryTree(root.left);
+        int b=diameterOfBinaryTree(root.right);
+        if(map.containsKey(root)){
+            return map.get(root);
+        }
+
+        int mid=level(root.left)+level(root.right);
+        map.put(root,mid+1);
+        if(root.left!=null)mid++;
+        if(root.right!=null)mid++;
+        map.put(root,Math.max(mid,Math.max(a,b)));
+        return map.get(root);
+    }
     public static int level(TreeNode root){
         if(root==null){
             return 0;
@@ -22,19 +41,5 @@ class Solution {
             return 0;
         }
         return 1+Math.max(level(root.left),level(root.right));
-    }
-    public int diameterOfBinaryTree(TreeNode root) {
-        if(root==null){
-            return 0;
-        }
-        int a=diameterOfBinaryTree(root.left);
-        int b=diameterOfBinaryTree(root.right);
-        int mid=level(root.left)+level(root.right);
-       if(root.left!=null)mid++;
-       if(root.right!=null)mid++;
-
-       return Math.max(a,Math.max(b,mid));
-
-        
     }
 }
