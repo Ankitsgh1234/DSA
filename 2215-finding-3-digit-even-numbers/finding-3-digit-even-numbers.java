2f14a3a150f5b07927e995e8e5c0ebef150b2433
Ankit kumar Singh <ankit2005sgh@gmail.com>
2026-06-08 00:08:46 +0530

    Time: 274 ms (36.26%) | Memory: 47.2 MB (33.54%) - LeetSync

---
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] findEvenNumbers(int[] digits) {
+    ArrayList<Integer> arr=new ArrayList<>();
+    Set<Integer> st = new HashSet<>();
+    int n = digits.length;
+
+    for(
+    int i = 0;i<n;i++)
+    {
+        if (digits[i] == 0)
+            continue;
+
+        for (int j = 0; j < n; j++) {
+            if (j == i)
+                continue;
+
+            for (int k = 0; k < n; k++) {
+                if (k == i || k == j)
+                    continue;
+
+                int num = digits[i] * 100
+                        + digits[j] * 10
+                        + digits[k];
+
+                if (num % 2 == 0) {
+                    st.add(num);
+                }
+            }
+        }
+    }
+    int[] primitiveArray = st.stream().mapToInt(Integer::intValue).toArray();
+    Arrays.sort(primitiveArray);
+    return primitiveArray;
+    }
+}
