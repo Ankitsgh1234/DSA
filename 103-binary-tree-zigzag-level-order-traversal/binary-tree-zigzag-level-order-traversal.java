fd1f63a467c082c5678e502351eca037d462854e
Ankit kumar Singh <ankit2005sgh@gmail.com>
2026-06-11 00:01:43 +0530

    Time: 1 ms (78.51%) | Memory: 43.8 MB (43.48%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -14,62 +14,58 @@
  * }
  */
 class Solution {
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        int n = height(root);
-        List<List<Integer>> ans=new ArrayList<>();
-        for (int i = 1; i <= height(root); i++) {
-            List<Integer> arr=new ArrayList<>();
-            if (i % 2 == 1)
-                nthorder(root, i,arr);
-            else
-                nthorder1(root, i,arr);
-                ans.add(arr);
-        }
-        return ans;
-    }
-
-    public static int height(TreeNode root) {
+      public static int height(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        // if((root.left==null&&root.right==null)){
-        // return 0;
-        // }
-
         return 1 + Math.max(height(root.left), height(root.right));
     }
-
-    public static void nthorder(TreeNode root, int n,List<Integer> arr) {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> arr=new ArrayList<>();
         if (root == null) {
-            return;
+            return arr;
         }
-        if (n == 1) {
-            arr.add(root.val);
-            
-            return;
+        for (int i = 1; i <= height(root); i++) {
+            List<Integer> t=new ArrayList<>();
+            if (i % 2 == 1)
+                nthorder(root, i,t);
+            else
+                nthorder1(root, i,t);
+            arr.add(t);
         }
-        
-
-            nthorder(root.left, n - 1,arr);
-            nthorder(root.right, n - 1,arr);
-        
-       
+        return arr;
     }
 
-    public static void nthorder1(TreeNode root, int n,List<Integer> arr) {
+    public static void nthorder(TreeNode root, int n,List<Integer> t) {
         if (root == null) {
             return;
         }
         if (n == 1) {
 
-            arr.add(root.val);
+            // System.out.print(root.val + " ");
+            t.add(root.val);
+            return;
+        }
+
+        nthorder(root.left, n - 1,t);
+        nthorder(root.right, n - 1,t);
+
+    }
+
+    public static void nthorder1(TreeNode root, int n,List<Integer> t) {
+        if (root == null) {
+            return;
+        }
+        if (n == 1) {
+
+            // System.out.print(root.val + " ");
+            t.add(root.val);
             return;
         }
         
 
-        nthorder1(root.right, n - 1,arr);
-        nthorder1(root.left, n - 1,arr);
+        nthorder1(root.right, n - 1,t);
+        nthorder1(root.left, n - 1,t);
         
-       
     }
 }
