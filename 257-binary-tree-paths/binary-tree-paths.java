211e0acabee75aac0ff85b96994ef9b83af2cd73
Ankit kumar Singh <ankit2005sgh@gmail.com>
2026-06-10 22:24:22 +0530

    Time: 6 ms (12.04%) | Memory: 49.8 MB (37.57%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -14,28 +14,27 @@
  * }
  */
 class Solution {
+    
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> l=new ArrayList<>();
-        leaf( root, l, "");
-        return l;
-        
+        List<String> lt=new ArrayList<>();
+        path(root,lt,"");
+        return lt; 
+
     }
-    public static void leaf(TreeNode root,List<String> l,String s){
-
+    public static void path(TreeNode root, List<String> lt,String s){
         if(root==null){
-            return;
-        }       
-        if(root.left==null&&root.right==null){
-            s+=root.val;
-            l.add(s);
-            return;
-
+            return ;
         }
-        leaf(root.left,l,s+root.val+"->");
-        leaf(root.right,l,s+root.val+"->");
-
-
-
+        if(root.left==null&&root.right==null){
+            s+=(root.val);
+            lt.add(s);
+            return ;
+        }
+        s+=(root.val);
+        s+=("->");
+        path(root.left,lt,s);
+        path(root.right,lt,s);
+        return ;
 
     }
 }
