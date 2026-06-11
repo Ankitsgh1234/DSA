e217120266e7820683060a608b211478b570fb72
Ankit kumar Singh <ankit2005sgh@gmail.com>
2026-06-12 00:19:08 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (31.90%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -15,24 +15,44 @@
  */
 class Solution {
     public void flatten(TreeNode root) {
-        if(root==null){
+        if (root == null) {
             return;
         }
-       TreeNode l=root.left;
-       TreeNode r=root.right;
-       flatten(root.left);
-       flatten(root.right);
-       root.right=root.left;
-       root.left=null;
-       TreeNode temp=l;
-       while(temp!=null&&temp.right!=null){
-        temp=temp.right;
-       }
-       if(temp!=null){
-        temp.right=r;
-       }
-       else{
-        root.right=r;
-       }
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.left != null) {
+
+                TreeNode pred = cur.left;
+                while (pred.right != null) {
+                    pred = pred.right;
+
+                }
+                pred.right = cur.right;
+                cur.right = cur.left;
+                cur.left = null;
+            }
+                cur = cur.right;
+        }
     }
+    // public void flatten(TreeNode root) {
+    //     if(root==null){
+    //         return;
+    //     }
+    //    TreeNode l=root.left;
+    //    TreeNode r=root.right;
+    //    flatten(root.left);
+    //    flatten(root.right);
+    //    root.right=root.left;
+    //    root.left=null;
+    //    TreeNode temp=l;
+    //    while(temp!=null&&temp.right!=null){
+    //     temp=temp.right;
+    //    }
+    //    if(temp!=null){
+    //     temp.right=r;
+    //    }
+    //    else{
+    //     root.right=r;
+    //    }
+    // }
 }
