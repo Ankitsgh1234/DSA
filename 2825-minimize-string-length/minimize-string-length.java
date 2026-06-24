465d8ffb0123ab0ec1cdf0e69679ec5b0022b0a9
Ankit kumar Singh <ankit2005sgh@gmail.com>
2026-06-24 16:41:05 +0530

    Time: 3 ms (100.00%) | Memory: 46.6 MB (68.52%) - LeetSync

---
+++ 2825-minimize-string-length/minimize-string-length.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+        int[] arr=new int[26];
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            arr[s.charAt(i)-'a']++;
+        }
+        int c=0;
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0)c++;
+        }
+        return c;
+    }
+}
