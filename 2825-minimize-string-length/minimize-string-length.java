413f42f7fe33b1e615347a548c7d6098517e5960
Ankit kumar Singh <ankit2005sgh@gmail.com>
2026-06-24 16:42:38 +0530

    Time: 7 ms (73.93%) | Memory: 46.7 MB (60.00%) - LeetSync

--- 2825-minimize-string-length/minimize-string-length.java
+++ 2825-minimize-string-length/minimize-string-length.java
@@ -1,14 +1,21 @@
 class Solution {
     public int minimizedStringLength(String s) {
-        int[] arr=new int[26];
+        // int[] arr=new int[26];
         int n=s.length();
+        // for(int i=0;i<n;i++){
+        //     arr[s.charAt(i)-'a']++;
+        // }
+        // int c=0;
+        // for(int i=0;i<26;i++){
+        //     if(arr[i]!=0)c++;
+        // }
+        // return c;
+
+
+        Set<Character> st=new HashSet<>();
         for(int i=0;i<n;i++){
-            arr[s.charAt(i)-'a']++;
+            st.add(s.charAt(i));
         }
-        int c=0;
-        for(int i=0;i<26;i++){
-            if(arr[i]!=0)c++;
-        }
-        return c;
+        return st.size();
     }
 }
